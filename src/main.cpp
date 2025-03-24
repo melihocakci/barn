34a1f2a0f7c26b3dad23d2bf761760aa442fab25
Ocakci, Melih (ADV D EU TR C&E CNT1) <melih.ocakci@siemens.com>
2025-03-24 16:39:03 +0300

    teto pear

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,47 @@
+#include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
-#include <string>
-#include <iostream>
 
-int main() {
-    sf::RenderWindow window;
-    window.create(sf::VideoMode({800, 600}), "SFML window");
-    std::string dummy;
-    std::getline(std::cin, dummy);
-    return 0;
+int main()
+{
+    // Create the main window
+    sf::RenderWindow window(sf::VideoMode({ 600, 600 }), "SFML window");
+
+    // Load a sprite to display
+    const sf::Texture texture("res/teto_pear.jpg");
+    sf::Sprite sprite(texture);
+
+    // Create a graphical text to display
+    const sf::Font font("res/Consolas.ttf");
+    sf::Text text(font, "I'm Tetobating so hard", 50);
+    text.setFillColor(sf::Color::Black);
+
+    // Load a music to play
+    sf::Music music("res/Kasane Teto - Teto territory.mp3");
+
+    // Play the music
+    music.play();
+
+    // Start the game loop
+    while (window.isOpen())
+    {
+        // Process events
+        while (const std::optional event = window.pollEvent())
+        {
+            // Close window: exit
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        // Clear screen
+        window.clear();
+
+        // Draw the sprite
+        window.draw(sprite);
+
+        // Draw the string
+        window.draw(text);
+
+        // Update the window
+        window.display();
+    }
 }
