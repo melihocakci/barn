07448fa8f0c914b520165823f112f426c1ede5f2
Ocakci, Melih (ADV D EU TR C&E CNT1) <melih.ocakci@siemens.com>
2025-03-30 10:04:09 +0300

    add entt library

--- src/main.cpp
+++ src/main.cpp
@@ -1,80 +1,109 @@
 #include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
 #include <steam/steam_api.h>
+#include <entt/entt.hpp>
 
 #include <map>
+#include <filesystem>
 
-constexpr int SCREEN_WIDTH = 800;
-constexpr int SCREEN_HEIGHT = 600;
-constexpr float BASE_SPEED = SCREEN_HEIGHT / 120;
+namespace mg {
+    constexpr int SCREEN_WIDTH = 800;
+    constexpr int SCREEN_HEIGHT = 600;
+    constexpr float BASE_SPEED = SCREEN_HEIGHT / 120;
 
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode({ SCREEN_WIDTH, SCREEN_HEIGHT }), "Teto Pear");
-    window.setFramerateLimit(60);
+    struct events {
+        bool right_pressed;
+        bool left_pressed;
+        bool up_pressed;
+        bool down_pressed;
+    };
 
-    const sf::Texture texture("res/teto_pear.jpg");
-    sf::Sprite sprite(texture);
+    events get_events(sf::RenderWindow& window);
 
-    const sf::Font font("res/Consolas.ttf");
-    sf::Text text(font, "I'm Tetobating so hard", SCREEN_HEIGHT / 12);
-    text.setFillColor(sf::Color::Black);
+    struct sprite {
+        sf::Texture txt;
+        sf::Sprite spr;
 
-    sf::Music music("res/Kasane Teto - Teto territory.mp3");
+        sprite(std::string_view sprite_file) : txt{ sprite_file }, spr{ txt } {
+            spr.scale({ 0.2, 0.2 });
+        }
+    };
+
+    enum class movement_type {
+        player,
+    };
+}
+
+mg::events mg::get_events(sf::RenderWindow& window) {
+    static mg::events events;
+
+    while (const std::optional event = window.pollEvent())
+    {
+        if (event->is<sf::Event::Closed>()) {
+            window.close();
+            exit(0);
+        }
+        else if (const auto* pressed_key = event->getIf<sf::Event::KeyPressed>()) {
+            if (pressed_key->scancode == sf::Keyboard::Scancode::Right) events.right_pressed = true;
+            else if (pressed_key->scancode == sf::Keyboard::Scancode::Left) events.left_pressed = true;
+            else if (pressed_key->scancode == sf::Keyboard::Scancode::Up) events.up_pressed = true;
+            else if (pressed_key->scancode == sf::Keyboard::Scancode::Down) events.down_pressed = true;
+        }
+        else if (const auto* pressed_key = event->getIf<sf::Event::KeyReleased>())
+        {
+            if (pressed_key->scancode == sf::Keyboard::Scancode::Right) events.right_pressed = false;
+            else if (pressed_key->scancode == sf::Keyboard::Scancode::Left) events.left_pressed = false;
+            else if (pressed_key->scancode == sf::Keyboard::Scancode::Up) events.up_pressed = false;
+            else if (pressed_key->scancode == sf::Keyboard::Scancode::Down) events.down_pressed = false;
+        }
+    }
+
+    return events;
+}
+
+int scene1(sf::RenderWindow& window) {
+    const sf::Texture bg_texture{ "res/teto_pear.jpg" };
+    const sf::Sprite background{ bg_texture };
+
+    sf::Music music{ "res/Kasane Teto - Teto territory.mp3" };
     music.play();
 
-    sf::RectangleShape player{ {SCREEN_WIDTH / 10, SCREEN_HEIGHT / 10} };
-    player.setFillColor(sf::Color::Black);
+    entt::registry registry;
+    entt::entity player_entity = registry.create();
+    registry.emplace<mg::sprite>(player_entity, "res/teto.png");
+    registry.emplace<mg::movement_type>(player_entity, mg::movement_type::player);
 
     while (window.isOpen())
     {
-        static sf::Vector2<int> player_direction = { 0, 0 };
-
-        while (const std::optional event = window.pollEvent())
-        {
-            std::map<sf::Keyboard::Scancode, sf::Vector2<int>> direction_vectors = {
-                    { sf::Keyboard::Scancode::Up, {0, -1} },
-                    { sf::Keyboard::Scancode::Down, {0, 1} },
-                    { sf::Keyboard::Scancode::Right, {1, 0} },
-                    { sf::Keyboard::Scancode::Left, {-1, 0} },
-            };
-
-            if (event->is<sf::Event::Closed>())
-            {
-                window.close();
-            }
-            else if (const auto* pressed_key = event->getIf<sf::Event::KeyPressed>())
-            {
-                auto search = direction_vectors.find(pressed_key->scancode);
-                if (search != direction_vectors.end()) {
-                    player_direction += search->second;
-                    if (player_direction.x < 0) player_direction.x = -1;
-                    if (player_direction.x > 0) player_direction.x = 1;
-                    if (player_direction.y < 0) player_direction.y = -1;
-                    if (player_direction.y > 0) player_direction.y = 1;
-                }
-            }
-            else if (const auto* pressed_key = event->getIf<sf::Event::KeyReleased>())
-            {
-                auto search = direction_vectors.find(pressed_key->scancode);
-                if (search != direction_vectors.end()) {
-                    player_direction -= search->second;
-                    if (player_direction.x < 0) player_direction.x = -1;
-                    if (player_direction.x > 0) player_direction.x = 1;
-                    if (player_direction.y < 0) player_direction.y = -1;
-                    if (player_direction.y > 0) player_direction.y = 1;
-                }
-            }
-        }
-
-        player.setPosition(player.getPosition() + sf::Vector2f{ player_direction.x * BASE_SPEED, player_direction.y * BASE_SPEED });
+        mg::get_events(window);
 
         window.clear();
+        window.draw(background);
 
-        window.draw(sprite);
-        window.draw(text);
-        window.draw(player);
+        registry.view<mg::movement_type, mg::sprite>().each(
+            [&window](const mg::movement_type& a, mg::sprite& sprite) {
+                mg::events events = mg::get_events(window);
+
+                if (events.left_pressed) sprite.spr.move(sf::Vector2f{ -mg::BASE_SPEED, 0 });
+                if (events.right_pressed) sprite.spr.move(sf::Vector2f{ mg::BASE_SPEED, 0 });
+                if (events.up_pressed) sprite.spr.move(sf::Vector2f{ 0, -mg::BASE_SPEED });
+                if (events.down_pressed) sprite.spr.move(sf::Vector2f{ 0, mg::BASE_SPEED });
+            }
+        );
+
+        registry.view<mg::sprite>().each(
+            [&window](const auto& sprite) { window.draw(sprite.spr); }
+        );
 
         window.display();
     }
+
+    return 0;
+}
+
+int main() {
+    sf::RenderWindow window{ sf::VideoMode{ {mg::SCREEN_WIDTH, mg::SCREEN_HEIGHT } }, "Teto Pear" };
+    window.setFramerateLimit(60);
+
+    if (scene1(window)) return 1;
 }
