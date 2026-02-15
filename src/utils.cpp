e8a4c51bb736bda9024e2fa63626f21b27cd7a92
Melih Ocakci <melihocakci@gmail.com>
2026-02-15 16:58:00 +0300

    separate asset loading functions

--- src/utils.cpp
+++ src/utils.cpp
@@ -5,74 +5,84 @@
 
 #include <future>
 
-template barn::asset<SDL_Texture> barn::get_asset(SDL_Renderer*, std::string_view);
-template barn::asset<MIX_Audio> barn::get_asset(MIX_Mixer*, std::string_view);
-
-template<typename asset_t, typename loader_t>
-	requires((std::is_same_v<asset_t, SDL_Texture>&& std::is_same_v<loader_t, SDL_Renderer>) || (std::is_same_v<asset_t, MIX_Audio> && std::is_same_v<loader_t, MIX_Mixer>))
-barn::asset<asset_t> barn::get_asset(loader_t* loader, std::string_view path) {
-	using element_t = asset_t*;
+barn::texture barn::get_texture(SDL_Renderer* renderer, std::string_view path) {
+	using element_t = SDL_Texture*;
 	using future_t = std::shared_future<element_t>;
 	using shared_ptr_t = std::shared_ptr<future_t>;
-	using weak_ptr_t = std::weak_ptr<future_t>;
+	using weak_ptr_t = shared_ptr_t::weak_type;
 
-	static std::unordered_map<std::string_view, weak_ptr_t> assets{};
+	static std::unordered_map<std::string_view, weak_ptr_t> textures{};
 
-	const auto it = assets.find(path);
-	if (it != assets.end()) {
+	const auto it = textures.find(path);
+	if (it != textures.end()) {
 		if (auto existing = it->second.lock()) {
 			return { existing };
 		}
 	}
 
-	shared_ptr_t asset;
-	if constexpr (std::is_same_v<asset_t, SDL_Texture>) {
-		asset = shared_ptr_t{
-			new future_t{},
-			[](future_t* future)
-			{
-				if (future->get()) {
-					SDL_DestroyTexture(future->get());
-				}
+	shared_ptr_t texture{
+		new future_t{},
+		[](future_t* future)
+		{
+			if (future->get()) {
+				SDL_DestroyTexture(future->get());
 			}
-		};
+		}
+	};
 
-		std::future<SDL_Surface*> surf_ftr = std::async(std::launch::async,
-			[path]() -> SDL_Surface*
-			{
-				return IMG_Load(path.data());
-			});
+	std::future<SDL_Surface*> surface_ftr = std::async(std::launch::async,
+		[path]() -> SDL_Surface*
+		{
+			return IMG_Load(path.data());
+		});
 
-		*asset = std::async(std::launch::deferred,
-			[loader, surf_ftr = std::move(surf_ftr)]() mutable -> SDL_Texture*
-			{
-				SDL_Surface* surf = surf_ftr.get();
-				SDL_Texture* texture = SDL_CreateTextureFromSurface(loader, surf);
-				SDL_DestroySurface(surf);
-				return texture;
-			});
-	}
-	else if constexpr (std::is_same_v<asset_t, MIX_Audio>) {
-		asset = shared_ptr_t{
-			new future_t{},
-			[](future_t* future)
-			{
-				if (future->get()) {
-					MIX_DestroyAudio(future->get());
-				}
+	*texture = std::async(std::launch::deferred,
+		[renderer, surf_ftr = std::move(surface_ftr)]() mutable -> SDL_Texture*
+		{
+			SDL_Surface* surface = surf_ftr.get();
+			if (!renderer || !surface) {
+				return nullptr;
 			}
-		};
+			SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+			SDL_DestroySurface(surface);
+			return texture;
+		});
 
-		*asset = std::async(std::launch::async,
-			[path]() -> MIX_Audio*
-			{
-				return MIX_LoadAudio(nullptr, path.data(), false);
-			});
-	}
-	else {
-		static_assert("Unsupported asset type");
+	textures[path] = texture;
+	return { texture };
+}
+
+barn::audio barn::get_audio(std::string_view path) {
+	using element_t = MIX_Audio*;
+	using future_t = std::shared_future<element_t>;
+	using shared_ptr_t = std::shared_ptr<future_t>;
+	using weak_ptr_t = shared_ptr_t::weak_type;
+
+	static std::unordered_map<std::string_view, weak_ptr_t> audios{};
+
+	const auto it = audios.find(path);
+	if (it != audios.end()) {
+		if (auto existing = it->second.lock()) {
+			return { existing };
+		}
 	}
 
-	assets[path] = asset;
-	return { asset };
+	shared_ptr_t audio{
+		new future_t{},
+		[](future_t* future)
+		{
+			if (future->get()) {
+				MIX_DestroyAudio(future->get());
+			}
+		}
+	};
+
+	*audio = std::async(std::launch::async,
+		[path]() -> MIX_Audio*
+		{
+			return MIX_LoadAudio(nullptr, path.data(), false);
+		});
+
+	audios[path] = audio;
+	return { audio };
 }
--- src/utils.h
+++ src/utils.h
@@ -20,15 +20,7 @@ namespace barn {
 		return vec / length(vec);
 	}
 
-	template<typename asset_t, typename loader_t>
-		requires ((std::is_same_v<asset_t, SDL_Texture>&& std::is_same_v<loader_t, SDL_Renderer>) || (std::is_same_v<asset_t, MIX_Audio> && std::is_same_v<loader_t, MIX_Mixer>))
-	barn::asset<asset_t> get_asset(loader_t* loader, std::string_view path);
+	barn::texture get_texture(SDL_Renderer* renderer, std::string_view path);
 
-	inline barn::texture get_texture(SDL_Renderer* renderer, std::string_view path) {
-		return get_asset<SDL_Texture>(renderer, path);
-	}
-
-	inline barn::audio get_audio(std::string_view path) {
-		return get_asset<MIX_Audio, MIX_Mixer>(static_cast<MIX_Mixer*>(nullptr), path);
-	}
+	barn::audio get_audio(std::string_view path);
 }
