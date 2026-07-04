28167fe3b34ecb887f6ab5eaa6fb8956df1eb259
Melih Ocakci <melihocakci@gmail.com>
2026-07-04 14:28:52 +0300

    add sidebars and background

--- src/constants.h
+++ src/constants.h
@@ -2,9 +2,9 @@
 
 namespace barn {
 	constexpr const char* PROJECT_NAME = "Barn";
-	constexpr int VIRTUAL_WIDTH_PIXELS = 1920;
+	constexpr int VIRTUAL_WIDTH_PIXELS = 1440;
 	constexpr int VIRTUAL_HEIGHT_PIXELS = 1080;
-	constexpr int VIRTUAL_WIDTH_METERS = 16;
+	constexpr int VIRTUAL_WIDTH_METERS = 12;
 	constexpr int VIRTUAL_HEIGHT_METERS = 9;
 	constexpr int PIXELS_PER_METER = VIRTUAL_HEIGHT_PIXELS / VIRTUAL_HEIGHT_METERS;
 	constexpr int BOX2D_SUB_STEP_COUNT = 4;
--- todo.md
+++ todo.md
@@ -1,4 +1,7 @@
 add input manager in context
+
 Add a character selection scene.
+
 Scenes should take "level" as parameter, along with player data from previous level and other things.
+
 For multiplayer, figure out how to represent current game state.
