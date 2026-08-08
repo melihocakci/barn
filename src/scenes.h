60b16c447ba0f53242800512e38e7ca2dfc331d3
Melih Ocakci <melihocakci@gmail.com>
2026-08-09 02:16:55 +0300

    refactor scene functions and improve menus

--- src/scenes.h
+++ src/scenes.h
@@ -6,12 +6,22 @@
 #include <SDL3_mixer/SDL_mixer.h>
 
 namespace barn {
+	enum class menu_state {
+		MAIN_MENU,
+		SETTINGS_MENU,
+		PAUSE_MENU,
+		START_GAME,
+		EXIT,
+	};
+
 	struct session {
 		std::vector<entity_def> players{};
 		barn::level level{};
 	};
 
-	int main_menu(barn::context& context);
+	barn::menu_state home_scene(barn::context& context);
 
-	int combat_scene(barn::context& context, barn::session& session);
+	std::optional<barn::session> lobby_scene(barn::context& context);
+
+	void combat_scene(barn::context& context, barn::session& session);
 }
